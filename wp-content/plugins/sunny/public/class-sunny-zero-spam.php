3c1c155981640f84eb1e2fb7cca30fba6cd27930
Phiphat Yoiphosai <rubjang@gmail.com>
2016-10-11 09:30:18 +0700

    sunny plugin

---
+++ wp-content/plugins/sunny/public/class-sunny-zero-spam.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package    Sunny
+ * @subpackage Sunny/public
+ * @author     Tang Rufus <rufus@wphuman.com>
+ * @since      1.4.11
+ *
+ */
+
+/**
+ * This class intergates with WordPress Zero Spam plugin.
+ */
+class Sunny_Zero_Spam extends Sunny_Abstract_Spam_Module {
+
+	/**
+	 * Set intergrated plugin name during class initialization
+	 *
+	 * @since 		1.5.0
+	 *
+	 */
+	public function set_intergrated_plugin_name() {
+		$this->intergrated_plugin_name = 'zero_spam';
+	}
+
+} // end Sunny_Zero_Spam
---
+++ wp-content/plugins/sunny/sunny.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * Dashboard. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * this starts the plugin.
+ *
+ * @link              https://www.wphuman.com
+ * @since             1.0.0
+ * @package           Sunny
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Sunny (Connecting CloudFlare and WordPress)
+ * Plugin URI:        https://www.wphuman.com/products/sunny/?utm_source=sunny&utm_medium=plugins&utm_term=Plugin%20URI&utm_campaign=wordpress%20org
+ * Description:       Automatically clear CloudFlare cache. And, protect your WordPress site at DNS level.
+ * Version:           1.5.4
+ * Author:            Tang Rufus
+ * Author URI:        https://www.wphuman.com/?utm_source=sunny&utm_medium=plugins&utm_term=Author%20URI&utm_campaign=wordpress%20org
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       sunny
+ * Domain Path:       /languages
+ */
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+*/
+function activate_sunny( $network_wide ) {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sunny-activator.php';
+	Sunny_Activator::activate( $network_wide );
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_sunny() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sunny-deactivator.php';
+	Sunny_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_sunny' );
+
+register_deactivation_hook( __FILE__, 'deactivate_sunny' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * dashboard-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-sunny.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.4.0
+ */
+function run_plugin_name() {
+
+	$plugin = new Sunny();
+	$plugin->run();
+
+}
+run_plugin_name();
