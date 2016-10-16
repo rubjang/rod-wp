1e9830dd3993d2407b64282661642b0985ffe555
Phiphat Yoiphosai <rubjang@gmail.com>
2016-10-16 22:26:49 +0700

    update wp-includes

--- wp-includes/class-walker-category-dropdown.php
+++ wp-includes/class-walker-category-dropdown.php
@@ -54,15 +54,19 @@ class Walker_CategoryDropdown extends Walker {
 			$value_field = 'term_id';
 		}
 
-		$output .= "\t<option class=\"level-$depth\" value=\"" . esc_attr( $category->{$value_field} ) . "\"";
+		$id_cat_notshow = array(84,85,86,87,88,89);
+    if(in_array($category->{$value_field},$id_cat_notshow)){ /* เช็ค id ที่ไม่ต้องการให้โชว์หมวดที่ต้องการ */
+		}else{
+			$output .= "\t<option class=\"level-$depth\" value=\"" . esc_attr( $category->{$value_field} ) . "\"";
 
-		// Type-juggling causes false matches, so we force everything to a string.
-		if ( (string) $category->{$value_field} === (string) $args['selected'] )
-			$output .= ' selected="selected"';
-		$output .= '>';
-		$output .= $pad.$cat_name;
-		if ( $args['show_count'] )
-			$output .= '&nbsp;&nbsp;('. number_format_i18n( $category->count ) .')';
-		$output .= "</option>\n";
+			// Type-juggling causes false matches, so we force everything to a string.
+			if ( (string) $category->{$value_field} === (string) $args['selected'] )
+				$output .= ' selected="selected"';
+			$output .= '>';
+			$output .= $pad.$cat_name;
+			if ( $args['show_count'] )
+				$output .= '&nbsp;&nbsp;('. number_format_i18n( $category->count ) .')';
+			$output .= "</option>\n";
+		}
 	}
 }
