ac54188c406e19e5017fc594390659d41904e7dc
Phiphat Yoiphosai <rubjang@gmail.com>
2016-10-19 23:11:12 +0700

    c7 edit

--- wp-content/themes/ThaiTheme-classified/includes/thaitheme-home/content7.php
+++ wp-content/themes/ThaiTheme-classified/includes/thaitheme-home/content7.php
@@ -7,28 +7,30 @@
 <?php if ( $my_query->have_posts() ) : $count = 1; while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 <article class="tt_list thaitheme post_news  <?php echo adext_adverts_css_classes( '', get_the_ID() ) ;?> <?php if($count % 2 == 0){echo " tt-2x";} if($count % 3 == 0){echo " tt-3x";} if($count % 4 == 0){echo " tt-4x";} if($count % 5 == 0){echo " tt-5x";} $count++; ?>">
 <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-<?php $image = thaitheme_img_list( get_the_ID() ) ?>
-<?php if($image) { ?>
-<div class="tt-img">
-<?php $tt_sticky = adext_adverts_css_classes('', get_the_ID() ) ; if($tt_sticky !=="") { ?> <div class="tt-img-sticky"></div> <?php } ?>
-<img src="<?php esc_attr_e($image) ?>?v=1.0" alt=""/>
-<div class="tt-opacity"></div>
-</div>
-<?php }else { ?>
-<div class="tt-img">
-<?php $tt_sticky = adext_adverts_css_classes('', get_the_ID() ) ; if($tt_sticky !=="") { ?> <div class="tt-img-sticky"></div> <?php } ?>
-<?php thaitheme_post_no_image(); ?>
-<div class="tt-opacity"></div>
-</div>
-<?php } ?>
-<div class="tt_desc">
-<p class="tt_ad_location"><i class="fa fa-map-marker"></i> <?php $thaitheme_mata_location = get_post_meta( get_the_ID(), "thaitheme_mata_location_province", true );  ?><?php esc_html_e( $thaitheme_mata_location ) ?> </p>
-<h3><?php esc_html_e( get_post( $post_id )->post_title ) ?></h3>
-<!--
-<p class="tt_ad_price"><?//php $price = get_post_meta( get_the_ID(), "adverts_price", true );  ?>
-	<?//php esc_html_e( adverts_price( $price ) ) ?> บาท</p>
--->
-</div>
+	<?php $image = thaitheme_img_list( get_the_ID() ) ?>
+	<?php if($image) { ?>
+		<div class="tt-img">
+		<?php $tt_sticky = adext_adverts_css_classes('', get_the_ID() ) ; if($tt_sticky !=="") { ?> <div class="tt-img-sticky"></div> <?php } ?>
+		<img src="<?php esc_attr_e($image) ?>?v=1.0" alt=""/>
+		<div class="tt-opacity"></div>
+		</div>
+	<?php }else { ?>
+		<div class="tt-img">
+		<?php $tt_sticky = adext_adverts_css_classes('', get_the_ID() ) ; if($tt_sticky !=="") { ?> <div class="tt-img-sticky"></div> <?php } ?>
+		<?php thaitheme_post_no_image(220,165); ?>
+		<div class="tt-opacity"></div>
+		</div>
+	<?php } ?>
+	<!--
+	<div class="tt_desc">
+	<p class="tt_ad_location"><i class="fa fa-map-marker"></i> <?//php $thaitheme_mata_location = get_post_meta( get_the_ID(), "thaitheme_mata_location_province", true );  ?><?//php esc_html_e( $thaitheme_mata_location ) ?> </p>
+	<h3><?//php esc_html_e( get_post( $post_id )->post_title ) ?></h3>
+
+	<p class="tt_ad_price"><?//php $price = get_post_meta( get_the_ID(), "adverts_price", true );  ?>
+		<?//php esc_html_e( adverts_price( $price ) ) ?> บาท</p>
+
+	</div>
+	-->
 </a>
 </article>
 <?php  endwhile;?><?php endif; wp_reset_query();?>
